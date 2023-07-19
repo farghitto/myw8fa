15a588fefa0373f365d2b6c2828277295bbc6326
Marco Prosperi <savemrc@gmail.com>
2023-07-19 10:56:40 +0200

    gitignore

--- Myw8ForAesthetics/Myw8ForAesthetics/settings.py
+++ Myw8ForAesthetics/Myw8ForAesthetics/settings.py
@@ -75,10 +75,14 @@ WSGI_APPLICATION = 'Myw8ForAesthetics.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MyOfficeAESviluppo',
+        'USER': 'myofficesviluppoAdmin',
+        'PASSWORD': 'ISIS$Office$isis2020',
+        'HOST': '192.168.76.200',
+        'PORT': '3306',
     }
-}
+}   
 
 
 # Password validation
