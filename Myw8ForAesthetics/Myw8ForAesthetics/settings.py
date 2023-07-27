2177587744170d43064b83125233fefb6bd333c3
Marco Prosperi <savemrc@gmail.com>
2023-07-27 13:01:07 +0200

    prova

--- Myw8ForAesthetics/Myw8ForAesthetics/settings.py
+++ Myw8ForAesthetics/Myw8ForAesthetics/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'utente',
 ]
 
 MIDDLEWARE = [
@@ -103,6 +106,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
