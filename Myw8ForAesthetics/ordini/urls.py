125406de58982c437cd10d3378ad1e3b43668ac6
farghitto <farghito@hotmail.com>
2023-10-05 23:43:57 +0200

    listini

--- Myw8ForAesthetics/ordini/urls.py
+++ Myw8ForAesthetics/ordini/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import sceltaprogramma
+from .views import sceltaprogramma, sceltalistino
 
 app_name = 'ordini'
 
 urlpatterns = [
    path('sceltaprogramma',  sceltaprogramma, name='scelta_programma'),
-
+   path('sceltalistino/<int:id>/',  sceltalistino, name='scelta_listino'),
 ]
--- Myw8ForAesthetics/ordini/views.py
+++ Myw8ForAesthetics/ordini/views.py
@@ -9,6 +9,7 @@ from Myw8ForAesthetics.decorators import handle_exceptions, handle_error_respons
 
 @handle_exceptions
 def sceltaprogramma(request):
+    
 
     url_backend = settings.BASE_URL + 'listini/lista/'
     headers = {"Authorization": f"Token {request.session['auth_token']}"}
@@ -18,7 +19,24 @@ def sceltaprogramma(request):
     elif response.status_code >= 400:
         return redirect('erroreserver', status_code=response.status_code, text=response.text)
 
-    print(listini)
+    context = {'dati': listini}
+
+    return render(request, 'ordini/scelta_gruppo.html', context)
+
+
+@handle_exceptions
+def sceltalistino(request, id):
+    
+
+    url_backend = settings.BASE_URL + 'listini/sceltaprogrammi/' + str(id)
+    headers = {"Authorization": f"Token {request.session['auth_token']}"}
+    response = requests.get(url_backend, headers=headers)
+    if response.status_code == 200:
+        listini = response.json()
+    elif response.status_code >= 400:
+        return redirect('erroreserver', status_code=response.status_code, text=response.text)
+
     context = {'dati': listini}
 
     return render(request, 'ordini/scelta_programma.html', context)
+
